26e80978e44cf1fa5f0cbe15259f1538c07e6eb4
Scott Cummings <scottcummings@Scotts-MacBook-Pro.local>
2022-05-02 14:44:47 -0700

    Implemented AddTopic and AddClass.

--- db_utils.py
+++ db_utils.py
@@ -22,17 +22,51 @@ class db_utils():
         # print("connection made..")
 
     # TODO: implement add class
-    def add_class(self, class_name):
-        sql = "INSERT INTO Class VALUES (%s);"
+    def add_class(self, class_name, topic_list):
+        sql_insert = "INSERT INTO Class (className) VALUES (%s);"
         vals = class_name
-        mycursor.execute(sql,vals)
-        mydb.commit()
-        print(mycursor.rowcount,"was inserted.")
+        topics_that_exist = []
+
+        for topics in topic_list: 
+            sql_select = "SELECT topicID FROM Topic WHERE TopicName = "+topics
+            mycursor.execute(sql_select)
+            results = mycursor.fetchall()
+            for x in results: 
+                print(x)
+            for x in results: 
+                topics_that_exist.append(x)
+            #Here we need to check that the topics exist before we add them to the ClassTopics table. 
+            mycursor.execute(sql_insert,vals)
+            mydb.commit()
+            print(mycursor.rowcount,"was inserted.")
+
+            
+        mycursor.execute(sql_insert,vals)
+        classIDQuery = "SELECT classID FROM Class WHERE className = "+class_name
+        mycursor.execute(classIDQuery)
+        classID = mycursor.fetchall()
+        for topics in topics_that_exist:
+            sql_insert = "INSERT INTO ClassTopic (classID,topicID) VALUES (%s,%s)"
+            vals = (classID,topics)
+            mycursor.execute(sql_insert,vals)
+            
+
+        
 
 
     #TODO: implement add topic
     def add_topic(self, topic_name: str):
-        return    
+        sql_insert = "INSERT INTO Topic VALUES (%s);"
+        vals = topic_name
+        mycursor.execute(sql_insert,vals)
+        mydb.commit()
+        print(mycursor.rowcount,"was inserted.")
+
+    
+    #def set_up_user(self,)
+
+        
+        
 
     # close connection
     def destructor(self):

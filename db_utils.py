8a55cd7dd6be40792ba89463a374ba6cdf4b8366
Scott Cummings <scottcummings@Scotts-MacBook-Pro.local>
2022-05-02 15:37:08 -0700

    Reformatted addig class topics.

--- db_utils.py
+++ db_utils.py
@@ -21,52 +21,45 @@ class db_utils():
         # self.cursor = self.db.cursor()
         # print("connection made..")
 
-    # TODO: implement add class
-    def add_class(self, class_name, topic_list):
+
+    def add_class(self, class_name):
         sql_insert = "INSERT INTO Class (className) VALUES (%s);"
         vals = class_name
-        topics_that_exist = []
-
-        for topics in topic_list: 
-            sql_select = "SELECT topicID FROM Topic WHERE TopicName = "+topics
-            mycursor.execute(sql_select)
-            results = mycursor.fetchall()
-            for x in results: 
-                print(x)
-            for x in results: 
-                topics_that_exist.append(x)
-            #Here we need to check that the topics exist before we add them to the ClassTopics table. 
-            mycursor.execute(sql_insert,vals)
-            mydb.commit()
-            print(mycursor.rowcount,"was inserted.")
-
-            
         mycursor.execute(sql_insert,vals)
-        classIDQuery = "SELECT classID FROM Class WHERE className = "+class_name
-        mycursor.execute(classIDQuery)
-        classID = mycursor.fetchall()
-        for topics in topics_that_exist:
-            sql_insert = "INSERT INTO ClassTopic (classID,topicID) VALUES (%s,%s)"
-            vals = (classID,topics)
-            mycursor.execute(sql_insert,vals)
+        mydb.commit()
+        print(mycursor.rowcount,"was inserted.")
             
 
-        
-
-
-    #TODO: implement add topic
-    def add_topic(self, topic_name: str):
-        sql_insert = "INSERT INTO Topic VALUES (%s);"
+    def add_topic(self, topic_name): 
+        sql_insert = "INSERT INTO Topic (topicName) VALUES (%s);"
         vals = topic_name
         mycursor.execute(sql_insert,vals)
         mydb.commit()
         print(mycursor.rowcount,"was inserted.")
+        
+    def add_class_topic(self, class_name: str, topic_names):
+        for name in topic_names:
+            sql_select = "SELECT topicID FROM Topic WHERE topicName = "+name+";"
+            mycursor.execute(sql_select)
+            topicID = mycursor.fetchall()
+            if (topicID != ""): #ie the topic does not exist
+                self.add_topic(name)
+                sql_select = "SELECT topicID FROM Topic WHERE topicName = "+name+";"
+                mycursor.execute(sql_select)
+                topicID = mycursor.fetchall()
+
+            sql_insert = "INSERT INTO ClassTopic (classID,topicID) VALUES (%s,%s);"
+            classIDSelect = "SELECT classID FROM Class WHERE className ="+class_name+";"
+            mycursor.execute(classIDSelect)
+            classID = mycursor.fetchall()
+            vals = (classID,topicID)
+            mycursor.execute(sql_insert,vals)
+            
 
     
     #def set_up_user(self,)
 
-        
-        
+   
 
     # close connection
     def destructor(self):

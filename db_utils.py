e53a8a3fb1fe99f2bb25b09cd62ffc98507b7390
Scott Cummings <scottcummings@Scotts-MacBook-Pro.local>
2022-05-02 20:14:56 -0700

    changed database name in db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -4,7 +4,7 @@ mydb = mysql.connector.connect(host="localhost",
 user="root",
 password="Sentry8949254816", # y'all pls don't hack meeeee
 auth_plugin='mysql_native_password',
-database="RideShare")
+database="WizBot")
 
 mycursor = mydb.cursor()
 
@@ -29,32 +29,6 @@ class db_utils():
         mydb.commit()
         print(mycursor.rowcount,"was inserted.")
             
-
-    def add_topic(self, topic_name): 
-        sql_insert = "INSERT INTO Topic (topicName) VALUES (%s);"
-        vals = topic_name
-        mycursor.execute(sql_insert,vals)
-        mydb.commit()
-        print(mycursor.rowcount,"was inserted.")
-        
-
-    def add_class_topic(self, class_name: str, topic_names):
-        for name in topic_names:
-            sql_select = "SELECT topicID FROM Topic WHERE topicName = "+name+";"
-            mycursor.execute(sql_select)
-            topicID = mycursor.fetchall()
-            if (topicID != ""): #ie the topic does not exist
-                self.add_topic(name)
-                sql_select = "SELECT topicID FROM Topic WHERE topicName = "+name+";"
-                mycursor.execute(sql_select)
-                topicID = mycursor.fetchall()
-
-            sql_insert = "INSERT INTO ClassTopic (classID,topicID) VALUES (%s,%s);"
-            classIDSelect = "SELECT classID FROM Class WHERE className ="+class_name+";"
-            mycursor.execute(classIDSelect)
-            classID = mycursor.fetchall()
-            vals = (classID,topicID)
-            mycursor.execute(sql_insert,vals)
 
     
     #def set_up_user(self,)
